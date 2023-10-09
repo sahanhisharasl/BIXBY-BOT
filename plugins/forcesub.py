1d2341bc43ec028877855d6c6afc23c4eb520b12
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-10-09 20:39:05 +0530

    Fix✅

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -7,7 +7,6 @@
 from pyrogram import Client, filters, enums 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
-from config import Config
 from helper.database import db
 from info import AUTH_CHANNEL
 
@@ -27,7 +26,7 @@ async def not_subscribed(_, client, message):
     
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.AUTH_CHANNEL}") ]]
+    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{AUTH_CHANNEL}") ]]
     text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
