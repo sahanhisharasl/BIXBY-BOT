c602d1f2fcf790974e5214cc1bd9011e0692149b
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-10-11 10:11:23 +0530

    Try New✅

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://graph.org/file/0443b7c2c75ecd0e20712.jpg https://graph.org/file/9dbd024664e2b558cc988.jpg https://graph.org/file/a1dd68cb33bf75c663bef.jpg https://graph.org/file/37d2fbad7975bda9934c5.jpg https://graph.org/file/b301bdbb7641c9d76dc78.jpg')).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
