890cf2b3dc402290c7bf83784b217262011f9d70
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-10-13 21:13:08 +0530

    Update sticker_id.py

--- plugins/ExtraMods/sticker_id.py
+++ plugins/ExtraMods/sticker_id.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command(["stickerid"]))
+@Client.on_message(filters.command(["stickerid"], PREFIX))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
        await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
