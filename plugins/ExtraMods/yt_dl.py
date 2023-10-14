e617a3c1dd5717f948faee7870c0eaf208a5048d
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-10-14 11:15:08 +0530

    🌟

--- plugins/ExtraMods/yt_dl.py
+++ plugins/ExtraMods/yt_dl.py
@@ -12,9 +12,9 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
+from info inport PREFIX
 
-
-@Client.on_message(filters.command(['song', 'mp3']) & filters.private)
+@Client.on_message(filters.command(["song", "mp3"], PREFIX) & filters.private)
 async def song(client, message):
     user_id = message.from_user.id 
     user_name = message.from_user.first_name 
@@ -84,7 +84,7 @@ def get_text(message: Message) -> [None,str]:
         return None
 
 
-@Client.on_message(filters.command(["video", "mp4"]))
+@Client.on_message(filters.command(["video", "mp4"], PREFIX))
 async def vsong(client, message: Message):
     urlissed = get_text(message)
     pablo = await client.send_message(message.chat.id, f"**𝙵𝙸𝙽𝙳𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚅𝙸𝙳𝙴𝙾** `{urlissed}`")
