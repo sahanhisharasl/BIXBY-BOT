882a8783762b4f54370a794c5241d4248c71d0b2
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-10-15 11:59:32 +0530

    🌟

--- plugins/ExtraMods/password.py
+++ plugins/ExtraMods/password.py
@@ -21,5 +21,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mᴋɴ Bᴏᴛᴢ™️', url='https://t.me/dxmodsupdates')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇꜱ', url='https://t.me/dxmodsupdates')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
