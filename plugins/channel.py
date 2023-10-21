f8de07f4a901f87683d95a6b38dce66ddf5c610b
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-10-21 23:18:44 +0530

    ᴄᴏʀᴇ ᴜᴘᴅᴀᴛᴇ ᴠ5.0 🔥

---
+++ plugins/channel.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2023 DX-MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
+from pyrogram import Client, filters
+from info import CHANNELS
+from database.ia_filterdb import save_file
+
+media_filter = filters.document | filters.video | filters.audio
+
+
+@Client.on_message(filters.chat(CHANNELS) & media_filter)
+async def media(bot, message):
+    """Media Handler"""
+    for file_type in ("document", "video", "audio"):
+        media = getattr(message, file_type, None)
+        if media is not None:
+            break
+    else:
+        return
+
+    media.file_type = file_type
+    media.caption = message.caption
+    await save_file(media)
