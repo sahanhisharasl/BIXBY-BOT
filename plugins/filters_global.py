c62b2d6cc76bf7d664a0bad7ae31fe062b635c93
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-10-21 23:10:50 +0530

    ᴄᴏʀᴇ ᴜᴘᴅᴀᴛᴇ ᴠ5.0 🔥

--- plugins/filters_global.py
+++ plugins/filters_global.py
@@ -7,16 +7,19 @@
 import io
 from pyrogram import filters, Client, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.filters_mdb import(
+   add_filter,
+   get_filters,
+   delete_filter,
+   count_filters
+)
 
-from database.filters_mdb import add_filter, get_filters, delete_filter, count_filters
-from database.gfilters_mdb import add_gfilter, get_gfilters, delete_gfilter, count_gfilters, del_allg
 from database.connections_mdb import active_connection
 from utils import get_file_id, parser, split_quotes
-from info import ADMINS, PREFIX
+from info import ADMINS
 
 
-
-@Client.on_message(filters.command(["filter", "add"], PREFIX) & filters.incoming)
+@Client.on_message(filters.command(['filter', 'add']) & filters.incoming)
 async def addfilter(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
@@ -66,7 +69,7 @@ async def addfilter(client, message):
         return
 
     if (len(extracted) >= 2) and not message.reply_to_message:
-        reply_text, btn, alert = parser(extracted[1], text, "alertmessage")
+        reply_text, btn, alert = parser(extracted[1], text)
         fileid = None
         if not reply_text:
             await message.reply_text("You cannot have buttons alone, give some text to go with it!", quote=True)
@@ -94,7 +97,7 @@ async def addfilter(client, message):
         try:
             msg = get_file_id(message.reply_to_message)
             fileid = msg.file_id if msg else None
-            reply_text, btn, alert = parser(extracted[1], text, "alertmessage") if message.reply_to_message.sticker else parser(message.reply_to_message.caption.html, text, "alertmessage")         
+            reply_text, btn, alert = parser(extracted[1], text) if message.reply_to_message.sticker else parser(message.reply_to_message.caption.html, text)
         except:
             reply_text = ""
             btn = "[]"
@@ -102,7 +105,7 @@ async def addfilter(client, message):
     elif message.reply_to_message and message.reply_to_message.text:
         try:
             fileid = None
-            reply_text, btn, alert = parser(message.reply_to_message.text.html, text, "alertmessage")
+            reply_text, btn, alert = parser(message.reply_to_message.text.html, text)
         except:
             reply_text = ""
             btn = "[]"
@@ -119,7 +122,7 @@ async def addfilter(client, message):
     )
 
 
-@Client.on_message(filters.command(["viewfilters", "filters"], PREFIX) & filters.incoming)
+@Client.on_message(filters.command(['viewfilters', 'filters']) & filters.incoming)
 async def get_all(client, message):
     
     chat_type = message.chat.type
@@ -127,6 +130,7 @@ async def get_all(client, message):
     if not userid:
         return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     if chat_type == enums.ChatType.PRIVATE:
+        userid = message.from_user.id
         grpid = await active_connection(str(userid))
         if grpid is not None:
             grp_id = grpid
@@ -182,7 +186,7 @@ async def get_all(client, message):
         parse_mode=enums.ParseMode.MARKDOWN
     )
         
-@Client.on_message(filters.command(["del"], PREFIX) & filters.incoming)
+@Client.on_message(filters.command('del') & filters.incoming)
 async def deletefilter(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
@@ -190,7 +194,7 @@ async def deletefilter(client, message):
     chat_type = message.chat.type
 
     if chat_type == enums.ChatType.PRIVATE:
-        grpid = await active_connection(str(userid))
+        grpid  = await active_connection(str(userid))
         if grpid is not None:
             grp_id = grpid
             try:
@@ -201,7 +205,6 @@ async def deletefilter(client, message):
                 return
         else:
             await message.reply_text("I'm not connected to any groups!", quote=True)
-            return
 
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         grp_id = message.chat.id
@@ -234,7 +237,7 @@ async def deletefilter(client, message):
     await delete_filter(message, query, grp_id)
         
 
-@Client.on_message(filters.command(["delall"], PREFIX) & filters.incoming)
+@Client.on_message(filters.command('delall') & filters.incoming)
 async def delallconfirm(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
@@ -242,7 +245,7 @@ async def delallconfirm(client, message):
     chat_type = message.chat.type
 
     if chat_type == enums.ChatType.PRIVATE:
-        grpid = await active_connection(str(userid))
+        grpid  = await active_connection(str(userid))
         if grpid is not None:
             grp_id = grpid
             try:
@@ -262,7 +265,6 @@ async def delallconfirm(client, message):
     else:
         return
 
-
     st = await client.get_chat_member(grp_id, userid)
     if (st.status == enums.ChatMemberStatus.OWNER) or (str(userid) in ADMINS):
         await message.reply_text(
@@ -272,139 +274,5 @@ async def delallconfirm(client, message):
                 [InlineKeyboardButton(text="CANCEL",callback_data="delallcancel")]
             ]),
             quote=True
-        )
-
-
-# Kanged from https://github.com/KDBotz
-@Client.on_message(filters.command(["gfilter", "addg"], PREFIX) & filters.incoming & filters.user(ADMINS))
-async def addgfilter(client, message):
-    args = message.text.html.split(None, 1)
-
-    if len(args) < 2:
-        await message.reply_text("Command Incomplete :(", quote=True)
-        return
-
-    extracted = split_quotes(args[1])
-    text = extracted[0].lower()
-
-    if not message.reply_to_message and len(extracted) < 2:
-        await message.reply_text("Add some content to save your filter!", quote=True)
-        return
-
-    if (len(extracted) >= 2) and not message.reply_to_message:
-        reply_text, btn, alert = parser(extracted[1], text, "galert")
-        fileid = None
-        if not reply_text:
-            await message.reply_text("You cannot have buttons alone, give some text to go with it!", quote=True)
-            return
-
-    elif message.reply_to_message and message.reply_to_message.reply_markup:
-        try:
-            rm = message.reply_to_message.reply_markup
-            btn = rm.inline_keyboard
-            msg = get_file_id(message.reply_to_message)
-            if msg:
-                fileid = msg.file_id
-                reply_text = message.reply_to_message.caption.html
-            else:
-                reply_text = message.reply_to_message.text.html
-                fileid = None
-            alert = None
-        except:
-            reply_text = ""
-            btn = "[]" 
-            fileid = None
-            alert = None
-
-    elif message.reply_to_message and message.reply_to_message.media:
-        try:
-            msg = get_file_id(message.reply_to_message)
-            fileid = msg.file_id if msg else None
-            reply_text, btn, alert = parser(extracted[1], text, "galert") if message.reply_to_message.sticker else parser(message.reply_to_message.caption.html, text, "galert")
-        except:
-            reply_text = ""
-            btn = "[]"
-            alert = None
-    elif message.reply_to_message and message.reply_to_message.text:
-        try:
-            fileid = None
-            reply_text, btn, alert = parser(message.reply_to_message.text.html, text, "galert")
-        except:
-            reply_text = ""
-            btn = "[]"
-            alert = None
-    else:
-        return
-
-    await add_gfilter('gfilters', text, reply_text, btn, fileid, alert)
-
-    await message.reply_text(
-        f"GFilter for  `{text}`  added",
-        quote=True,
-        parse_mode=enums.ParseMode.MARKDOWN
-    )
-
-
-@Client.on_message(filters.command(["viewgfilters", "gfilters"], PREFIX) & filters.incoming & filters.user(ADMINS))
-async def get_all_gfilters(client, message):
-    texts = await get_gfilters('gfilters')
-    count = await count_gfilters('gfilters')
-    if count:
-        gfilterlist = f"Total number of gfilters : {count}\n\n"
-
-        for text in texts:
-            keywords = " ×  `{}`\n".format(text)
-
-            gfilterlist += keywords
-
-        if len(gfilterlist) > 4096:
-            with io.BytesIO(str.encode(gfilterlist.replace("`", ""))) as keyword_file:
-                keyword_file.name = "keywords.txt"
-                await message.reply_document(
-                    document=keyword_file,
-                    quote=True
-                )
-            return
-    else:
-        gfilterlist = f"There are no active gfilters."
-
-    await message.reply_text(
-        text=gfilterlist,
-        quote=True,
-        parse_mode=enums.ParseMode.MARKDOWN
-    )
-        
-@Client.on_message(filters.command(["delg"], PREFIX) & filters.incoming & filters.user(ADMINS))
-async def deletegfilter(client, message):
-    try:
-        cmd, text = message.text.split(" ", 1)
-    except:
-        await message.reply_text(
-            "<i>Mention the gfiltername which you wanna delete!</i>\n\n"
-            "<code>/delg gfiltername</code>\n\n"
-            "Use /viewgfilters to view all available gfilters",
-            quote=True
-        )
-        return
-
-    query = text.lower()
-
-    await delete_gfilter(message, query, 'gfilters')
-
-
-@Client.on_message(filters.command(["delallg"], PREFIX) & filters.user(ADMINS))
-async def delallgfill(client, message):
-    await message.reply_text(
-            f"Do you want to continue??",
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="YES",callback_data="gconforme")],
-                [InlineKeyboardButton(text="CANCEL",callback_data="close_data")]
-            ]),
-            quote=True
-        )
-
-
-@Client.on_callback_query(filters.regex("gconforme"))
-async def dellacbd(client, message):
-    await del_allg(message.message, 'gfilters')
-    return await message.reply("👍 Done")
+            )
+    
